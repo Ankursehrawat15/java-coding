e458801961120fe7bd6e4391785a4f90ef34dd22
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-05-24 14:33:46 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,7 +1,7 @@
 public class Pattern {
 
     public static void main(String[] args) {
-        int rows = 5;
+        int rows = 4;
 
         for(int i = 1; i <= rows; ++i) {
             for(int j = 1; j <= i; ++j) {
